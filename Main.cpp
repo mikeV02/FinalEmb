a37ecc63c6011906ba1c79b51ef7877f51371454
Miguel <mikevelasquez02@users.noreply.github.com>
2016-11-16 23:22:50 -0600

    KEY 0 FULLY IMPLEMENTED

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdint.h>
 #include "PIO_LED.h"
@@ -15,10 +14,6 @@ void* first_move(void* Sp)//void* first_move(void*)
 
     Spider->SetSpeed(80);
 
-    //Spider.Reset();
-
-    //Spider.WakeUp();
-
     std::cout<<"\nMove Forward\n";
     Spider->MoveForward(10);
 
@@ -31,22 +26,39 @@ void* first_move(void* Sp)//void* first_move(void*)
 
     std::cout<<"\nMove Forward Again\n";
     Spider->MoveForward(10);
+    
+    Spider->Reset();
 
     return NULL;
 }
 
-void* LEDs(void*)
+void* LEDsL(void*)
 {
+
     CPIO_LED LED_PIO;
 
-    uint32_t tickCount = OS_GetTickCount(); 
+    uint32_t tickCount = OS_GetTickCount();
     int led = 1;
     int frequency = 8;            
         
     while (true)
     {
+
+        #define handle_error_en(en, msg) \
+             do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
+        int s, t;
+
+        t = pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+
+        s = pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+        if (s != 0)
+            handle_error_en(s, "pthread_setcancelstate");
+
+        if (t != 0)
+            handle_error_en(s, "pthread_setcanceltype");
+
         if(OS_GetTickCount()-tickCount > OS_TicksPerSecond()/frequency) {
-            tickCount = OS_GetTickCount(); 
+            tickCount = OS_GetTickCount();
                 led = led << 1;
 
             if(led > 256) {
@@ -61,21 +73,77 @@ void* LEDs(void*)
     return NULL;
 }
 
+void* LEDsR(void*)
+{
+
+    CPIO_LED LED_PIO;
+
+    uint32_t tickCount = OS_GetTickCount();
+    int led = 1;
+    int frequency = 8;            
+        
+    while (true)
+    {
+
+        #define handle_error_en(en, msg) \
+             do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
+        int s, t;
+
+        t = pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+
+        s = pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+        if (s != 0)
+            handle_error_en(s, "pthread_setcancelstate");
+
+        if (t != 0)
+            handle_error_en(s, "pthread_setcanceltype");
+
+        if(OS_GetTickCount()-tickCount > OS_TicksPerSecond()/frequency) {
+            tickCount = OS_GetTickCount();
+                led = led >> 1;
+
+            if(led < 1) {
+                        led = 256;
+                }
+
+            LED_PIO.SetLED(led);
+                        
+        }
+    }
+
+    return NULL;
+}
+
+void* exitProg(void* btn)
+{
+    
+    CPIO_BUTTON *BUTTON_PIO = (CPIO_BUTTON*) btn;
+    printf("Exit function\r\n");
+
+    while((BUTTON_PIO->GetBUTTON() & 0x03) != 0)
+    { }
+    exit(0);
+    return 0;
+}
+
 int main(int argc, char *argv[]){
 
     CPIO_LED LED_PIO;
     CPIO_BUTTON BUTTON_PIO;
     CSpider Spider;
-
-    Spider.SetSpeed(100);
-
-    //Spider.Reset();
-    std::cout<<"\n\nReset Done\n\n";
-
-    Spider.Abort();
-    Spider.Sleep();
-    Spider.WakeUp();
     
+    int buttons;
+    int prev_buttons;
+
+
+    printf("Before exit thread\r\n");
+    // Exit Program------------------------------------
+    //pthread_t Ext;
+    //pthread_create(&Ext, NULL, &exitProg, &BUTTON_PIO);
+    //-------------------------------------------------
+    printf("After exit thread\r\n");
+
+
     printf("Spider Init & Standup\r\n");
     if (!Spider.Init())
     {
@@ -87,32 +155,53 @@ int main(int argc, char *argv[]){
             printf("Spilder Standup failed\r\n");
     }
 
+
+    Spider.SetSpeed(100);
+
+    pthread_t ledsL;
+    pthread_create(&ledsL, NULL, &LEDsL, NULL); //LEDs Left thread
+    
     printf("Main Funtionality\n");
     while(true)
     {
+        prev_buttons = buttons;
+        buttons = BUTTON_PIO.GetBUTTON();
 
         if ((BUTTON_PIO.GetBUTTON() & 0x01)== 0) //KEY0 FIRST MOVE
         {
-            pthread_t t1, leds;
-            pthread_cancel(leds);
+            if(buttons != prev_buttons)
+            {
+                pthread_cancel(ledsL);
+                printf("\n\nLEFT LEDs has been Stopped\n\n");
 
-            pthread_create(&t1, NULL, &first_move, &Spider); //Move Thread
+                pthread_t t1, ledsR;
 
-            pthread_create(&leds, NULL, &LEDs, NULL); //LEDs thread
+                pthread_create(&t1, NULL, &first_move, &Spider); //Move Thread
 
-            //-----Stop LEDs thread
-            void* result;
-            pthread_join(t1,&result);
+                pthread_create(&ledsR, NULL, &LEDsR, NULL); //LEDs Right thread
 
-            pthread_cancel(leds);
-            //---------------------
 
+                //-----Stop LEDs thread
+                void* result;
+                pthread_join(t1,&result);
+                printf("Joining\r\n");
+                pthread_cancel(ledsR);
+                printf("\n\nLEDs has been Stopped 2\n\n");
+                //---------------------
+            }
         }
 
         if ((BUTTON_PIO.GetBUTTON() & 0x02)== 0) //KEY1
         {
+            if(buttons != prev_buttons){
             exit(0); //Key 1 Not implemented yet... Just exit
+            }
         }
     }
 return 0;
 }
+
+
+
+
+
